0041eacf9f8a537ad7a65caa4f60d6ae8c3fdaa1
obl <obl@danalock.com>
2024-10-15 08:20:40 +0200

    added table and changed the use from grid to a advanced grid for better visual.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { DashboardComponent } from "./dashboard/dashboard.component";
 import { AgentInformationComponent } from "./agent-information/agent-information.component";
 import { TeamInformationComponent } from "./team-information/team-information.component";
 import { NavBarComponent } from "./nav-bar/nav-bar.component";
-
+import {MatSortModule} from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, DashboardComponent, AgentInformationComponent, TeamInformationComponent, NavBarComponent],
+  imports: [RouterOutlet, DashboardComponent, AgentInformationComponent, TeamInformationComponent, NavBarComponent, MatSortModule, MatTableModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -2,14 +2,32 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {MatGridListModule} from '@angular/material/grid-list';
 import { NavBarComponent } from "../nav-bar/nav-bar.component";
+import { TeamInformationComponent } from "../team-information/team-information.component";
+import { NgFor } from '@angular/common';
 
+
+export interface Tile {
+  color: string;
+  cols: number;
+  rows: number;
+  text: string;
+}
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [RouterOutlet, MatGridListModule, NavBarComponent],
+  imports: [RouterOutlet, MatGridListModule, NavBarComponent, TeamInformationComponent, NgFor],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent {
 
+  tiles: Tile[] = [
+    {text: 'One', cols: 3, rows: 1, color: 'lightblue'},
+    {text: 'Two', cols: 1, rows: 2, color: 'lightgreen'},
+    {text: 'Three', cols: 1, rows: 1, color: 'lightpink'},
+    {text: 'Four', cols: 2, rows: 1, color: '#DDBDF1'},
+  ];
+
 }
+
+
