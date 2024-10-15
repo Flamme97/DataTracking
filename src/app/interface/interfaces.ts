a9d3cf9882a96fc55714c803255c02dfa4020dff
obl <obl@danalock.com>
2024-10-15 08:49:22 +0200

    added interaces and setup test env in prod code. To simply have some data that can show currently look.

---
+++ src/app/interface/interfaces.ts
@@ -0,0 +1,13 @@
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+}
+
+export interface TableCol {
+  position: number;
+  name: string;
+  sales: number,
+  procent: number,
+}
--- src/app/team-information/team-information.component.ts
+++ src/app/team-information/team-information.component.ts
@@ -2,13 +2,9 @@ import { Component, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {MatSort, MatSortModule} from '@angular/material/sort';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { PeriodicElement } from '../interface/interfaces';
+import { ELEMENT_DATA } from '../interface/TeamsColumn.interface';
 
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
 
 @Component({
   selector: 'app-team-information',
@@ -20,22 +16,12 @@ export interface PeriodicElement {
 export class TeamInformationComponent {
 
 
-    ELEMENT_DATA: PeriodicElement[] = [
-    {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-    {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-    {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-    {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-    {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-    {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-    {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-    {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-    {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-    {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-  ];
+    TableData: PeriodicElement[] = ELEMENT_DATA
+    
 
 
   displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = new MatTableDataSource(this.ELEMENT_DATA);
+  dataSource = new MatTableDataSource(this.TableData);
 
   @ViewChild(MatSort) sort!: MatSort ;
 
